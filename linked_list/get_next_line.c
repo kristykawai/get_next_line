67a7c7e798a1153b16dd55f82d938e10dfeaf710
kristykawai <kristy.kw.chan@gmail.com>
2023-09-09 22:12:31 +0930

    first commit folder restructure

---
+++ linked_list/get_next_line.c
@@ -0,0 +1,148 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchan <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 14:45:25 by kchan             #+#    #+#             */
+/*   Updated: 2023/05/27 17:32:35 by kchan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static t_list	*stash;
+	char			*line;
+
+	if (fd < 0 || BUFFER_SIZE < 0 || read(fd, &line, 0) < 0)
+	{
+		free_stash(stash);
+		stash = NULL;
+		return (NULL);
+	}
+	line = NULL;
+	read_and_stash(fd, &stash);
+	if (stash == NULL)
+		return (NULL);
+	extract_line(stash, &line);
+	clean_stash(&stash);
+	if (line[0] == '\0')
+	{
+		free_stash(stash);
+		stash = NULL;
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+
+void	read_and_stash(int fd, t_list **stash)
+{
+	char	*buf;
+	int		readed;
+
+	readed = 1;
+	while (!found_newline(*stash) && readed != 0)
+	{
+		buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (buf == NULL)
+			return ;
+		readed = read(fd, buf, BUFFER_SIZE);
+		if (readed == -1)
+		{
+			free(buf);
+			return ;
+		}
+		buf[readed] = '\0';
+		add_to_stash(stash, buf, readed);
+		free(buf);
+	}
+}
+
+void	add_to_stash(t_list **stash, char *buf, int readed)
+{
+	int		i;
+	t_list	*last;
+	t_list	*new_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (new_node == NULL)
+		return ;
+	new_node->next = NULL;
+	new_node->content = malloc(sizeof(char) * (readed + 1));
+	if (new_node->content == NULL)
+		return ;
+	i = 0;
+	while (buf[i] && i < readed)
+	{
+		new_node->content[i] = buf[i];
+		i++;
+	}
+	new_node->content[i] = '\0';
+	if (*stash == NULL)
+	{
+		*stash = new_node;
+		return ;
+	}
+	last = ft_lst_get_last(*stash);
+	last->next = new_node;
+}
+
+void	extract_line(t_list *stash, char **line)
+{
+	int	i;
+	int	j;
+
+	if (stash == NULL)
+		return ;
+	generate_line(line, stash);
+	if (*line == NULL)
+		return ;
+	j = 0;
+	while (stash)
+	{
+		i = 0;
+		while (stash->content[i] && stash->content[i] != '\n')
+		{
+			(*line)[j++] = stash->content[i++];
+		}
+		if (stash->content[i] == '\n')
+		{
+			(*line)[j++] = stash->content[i];
+		}
+		stash = stash->next;
+	}
+	(*line)[j] = '\0';
+}
+
+void	clean_stash(t_list **stash)
+{
+	t_list	*last;
+	t_list	*clean_node;
+	int		i;
+	int		j;
+
+	clean_node = malloc(sizeof(t_list));
+	if (stash == NULL || clean_node == NULL)
+		return ;
+	clean_node->next = NULL;
+	last = ft_lst_get_last(*stash);
+	i = 0;
+	while (last->content[i] && last->content[i] != '\n')
+		i++;
+	if (last->content && last->content[i] == '\n')
+		i++;
+	clean_node->content = malloc(sizeof(char)
+			* ((ft_strlen(last->content) - i) + 1));
+	if (clean_node->content == NULL)
+		return ;
+	j = 0;
+	while (last->content[i])
+		clean_node->content[j++] = last->content[i++];
+	clean_node->content[j] = '\0';
+	free_stash(*stash);
+	*stash = clean_node;
+}
